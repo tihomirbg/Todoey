20a890d6ca8165c69e7888cff319f86c1666c6ef
Tihomir Dimitrov <dimitrov@leandigitalsolutions.com>
2018-02-13 16:57:44 +0200

    Convert to MVC pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Tihomir Dimitrov on 2/13/18.
+//  Copyright © 2018 Tihomir Dimitrov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
